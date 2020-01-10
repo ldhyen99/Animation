87d06b10f4e6cf9c109def2151c74cbc91ec0a53
ldhyen99 <59736798+ldhyen99@users.noreply.github.com>
2020-01-10 22:09:02 +0700

    Add files via upload

---
+++ arctica/1.js
@@ -0,0 +1,22 @@
+﻿ $(function(){
+    khoimonan = $('.nhieumon').isotope({
+        // options
+        itemSelector: '.motmon',
+        layoutMode: 'masonry'
+      });
+
+      // layout Isotope after each image loads
+      khoimonan.imagesLoaded().progress( function() {
+        khoimonan.isotope('layout');
+      });
+      
+      $('.tieudect a').click(function() {
+          dulieu = $(this).data('monan'); //Lay du lieu ve, data-monan: truyen du lieu
+       //  console.log(dulieu);
+
+        khoimonan.isotope({ filter: dulieu });
+
+        return false;
+      });
+})  
+ 
